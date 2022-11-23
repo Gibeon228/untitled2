85af14aee82123ebd3584a77fcdfcda72fe871f8
Vladimir <VL97@mail.ru>
2022-11-24 01:03:31 +0300

    dz1 сделана

--- src/main/java/org/example/Cat.java
+++ src/main/java/org/example/Cat.java
@@ -5,12 +5,16 @@ public class Cat {
     private String name;
     private String color;
     private int age;
+    private int idMother;
+    private int idFather;
 
-    public Cat(int id, String name, String color, int age) {
+    public Cat(int id, String name, String color, int age, int idMother, int idFather) {
         this.id = id;
         this.name = name;
         this.color = color;
         this.age = age;
+        this.idMother = idMother;
+        this.idFather = idFather;
     }
 
     public void setName(String name) {
@@ -23,7 +27,7 @@ public class Cat {
 
     @Override
     public String toString() {
-        return "имя кота: " + this.name + "\nокрас кота: " + this.color + "\nколичество полных лет: " + this.age + "\n";
+        return "имя кота: " + this.name + "\nокрас кота: " + this.color + "\nколичество полных лет: " + this.age + "\n" + "Мать: " + this.idMother + "\n" + "Отец: " + this.idFather + "\n";
     }
 
     @Override
@@ -50,3 +54,45 @@ public class Cat {
         return id;
     }
 }
+
+
+
+
+//  добавить в main
+/*
+package org.example;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    private static Set<Cat> catSet;
+
+    public static void addCat(int id, String name, String color, int age) {
+        Cat cat = new Cat(id, name, color, age);
+        catSet.add(cat);
+    }
+
+    public static void main(String[] args) {
+        catSet = new HashSet<>();
+        addCat(1, "Barsik", "Black", 3);
+        addCat(2, "Filya", "black-white", 1);
+        addCat(1, "Barsik", "Black", 3);
+        addCat(1, "Barsik", "Black", 4);
+
+        for (Cat cat1 : catSet) {
+            System.out.println(cat1);
+            for (Cat cat2 : catSet) {
+                System.out.println(cat2);
+                System.out.println(cat1.equals(cat2));
+                System.out.println("----");
+            }
+            System.out.println("------------------------------------------------------");
+        }
+
+//        for (Cat cat1 : catSet) {
+//            System.out.println(cat1.hashCode());
+//        }
+    }
+
+ */
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,17 +6,18 @@ import java.util.Set;
 public class Main {
     private static Set<Cat> catSet;
 
-    public static void addCat(int id, String name, String color, int age) {
-        Cat cat = new Cat(id, name, color, age);
+    public static void addCat(int id, String name, String color, int age, int idMother, int idFather) {
+        Cat cat = new Cat(id, name, color, age, idMother, idFather);
         catSet.add(cat);
     }
 
     public static void main(String[] args) {
         catSet = new HashSet<>();
-        addCat(1, "Barsik", "Black", 3);
-        addCat(2, "Filya", "black-white", 1);
-        addCat(1, "Barsik", "Black", 3);
-        addCat(1, "Barsik", "Black", 4);
+        addCat(1, "Barsik", "Black", 3, 3, 2);
+        addCat(2, "Filya", "black-white", 1, 4, 7);
+        addCat(1, "Barsik", "Black", 3, 3, 2);
+        addCat(1, "Barsik", "Black", 4,3, 2 );
+        addCat(3, "Murka", "Black", 4,9, 11 );
 
         for (Cat cat1 : catSet) {
             System.out.println(cat1);
@@ -28,8 +29,8 @@ public class Main {
             System.out.println("------------------------------------------------------");
         }
 
-//        for (Cat cat1 : catSet) {
-//            System.out.println(cat1.hashCode());
-//        }
+        for (Cat cat1 : catSet) {
+            System.out.println(cat1.hashCode());
+        }
     }
 }
